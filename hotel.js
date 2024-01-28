abfd27d8e3b354c6951a5fbd7fc1c30ac0657f08
Deepak Kumar <dkkr30907@gmail.com>
2024-01-28 06:15:34 +0530

    hotelMadhuban

---
+++ hotel.js
@@ -0,0 +1,28 @@
+    
+let checkavaibility= ()=>
+alert(' All types of Room are available, Thankyou!')
+
+let spa= ()=>
+document.getElementById('pic').src="./images/spa1.webp"
+
+let restro= ()=>
+document.getElementById('pic').src="./images/spa2.webp"
+
+
+let pool= ()=>
+document.getElementById('pic').src="./images/s1.jpeg"
+
+
+let movie= ()=>
+document.getElementById('pic').src="./images/r1.jpeg"
+
+
+
+let club= ()=>
+document.getElementById('pic').src="./images/r2.webp"
+
+
+let hall= ()=>
+document.getElementById('pic').src="./images/1st-slide.jpeg"
+
+
